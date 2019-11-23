81eea6646cdbf714daa75a45841e797d397eefb6
Rajesh P <rajeshp@Rajeshs-MacBook-Pro.local>
2019-11-23 07:47:40 -0600

    Initial Commit

---
+++ sum_count_max.py
@@ -0,0 +1,11 @@
+number_list=[9.1, 8.8, 10.0, 7.7, 6.8, 8.0, 10.0, 8.1, 10.0, 9.9,15.0]
+
+print("Length of the list is :", len(number_list))
+
+
+print("Count of 10 in the list is :", number_list.count(10))
+
+print("Sum of values in the list is :", sum(number_list))
+
+print("Max  values in the list is :", max(number_list))
+
